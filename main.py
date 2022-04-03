d789fda451a59c158b8aeee332ed64d2e17033ec
Soumya Rudra <ajaxmemo@gmail.com>
2022-04-03 23:44:37 +0530

    first edition

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from flask import abort
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "abcd"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
